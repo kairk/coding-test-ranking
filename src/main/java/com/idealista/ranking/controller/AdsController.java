8100acc7e987f1769f5ff65be343db1dcfd863a1
kairk <vgc23740@gmail.com>
2021-04-29 20:23:03 +0200

    Create SwaggerConfiguration, add basic swagger info, Create global exception hadler, add domain generic exception, add business and service facade, add lombok and spring boot maven compatibility plugins

--- src/main/java/com/idealista/ranking/controller/AdsController.java
+++ src/main/java/com/idealista/ranking/controller/AdsController.java
@@ -1,23 +1,36 @@
 package com.idealista.ranking.controller;
 
+import com.idealista.ranking.business.AdsBusiness;
 import com.idealista.ranking.model.api.response.PublicAdResponse;
 import com.idealista.ranking.model.api.response.QualityAdResponse;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(Consts.API_VERSION + Consts.ADS_ENDPOINT)
+@Api(tags = "Advertisement")
 public class AdsController {
 
+    private final AdsBusiness adsBusiness;
+
+    @Autowired
+    public AdsController(AdsBusiness adsBusiness) {
+        this.adsBusiness = adsBusiness;
+    }
+
     /*
-    Yo, como encargado del equipo de gestión de calidad de los anuncios quiero asignar una puntuación a un anuncio para que los usuarios de idealista puedan ordenar anuncios de más completos a menos completos. La puntuación del anuncio es un valor entre 0 y 100 que se calcula teniendo encuenta las siguientes reglas:
-Si el anuncio no tiene ninguna foto se restan 10 puntos. Cada foto que tenga el anuncio proporciona 20 puntos si es una foto de alta resolución (HD) o 10 si no lo es.
-Que el anuncio tenga un texto descriptivo suma 5 puntos.
-El tamaño de la descripción también proporciona puntos cuando el anuncio es sobre un piso o sobre un chalet. En el caso de los pisos, la descripción aporta 10 puntos si tiene entre 20 y 49 palabras o 30 puntos si tiene 50 o mas palabras. En el caso de los chalets, si tiene mas de 50 palabras, añade 20 puntos.
-Que las siguientes palabras aparezcan en la descripción añaden 5 puntos cada una: Luminoso, Nuevo, Céntrico, Reformado, Ático.
-Que el anuncio esté completo también aporta puntos. Para considerar un anuncio completo este tiene que tener descripción, al menos una foto y los datos particulares de cada tipología, esto es, en el caso de los pisos tiene que tener también tamaño de vivienda, en el de los chalets, tamaño de vivienda y de jardín. Además, excepcionalmente, en los garajes no es necesario que el anuncio tenga descripción. Si el anuncio tiene todos los datos anteriores, proporciona otros 40 puntos.
-     */
+        Yo, como encargado del equipo de gestión de calidad de los anuncios quiero asignar una puntuación a un anuncio para que los usuarios de idealista puedan ordenar anuncios de más completos a menos completos. La puntuación del anuncio es un valor entre 0 y 100 que se calcula teniendo encuenta las siguientes reglas:
+    Si el anuncio no tiene ninguna foto se restan 10 puntos. Cada foto que tenga el anuncio proporciona 20 puntos si es una foto de alta resolución (HD) o 10 si no lo es.
+    Que el anuncio tenga un texto descriptivo suma 5 puntos.
+    El tamaño de la descripción también proporciona puntos cuando el anuncio es sobre un piso o sobre un chalet. En el caso de los pisos, la descripción aporta 10 puntos si tiene entre 20 y 49 palabras o 30 puntos si tiene 50 o mas palabras. En el caso de los chalets, si tiene mas de 50 palabras, añade 20 puntos.
+    Que las siguientes palabras aparezcan en la descripción añaden 5 puntos cada una: Luminoso, Nuevo, Céntrico, Reformado, Ático.
+    Que el anuncio esté completo también aporta puntos. Para considerar un anuncio completo este tiene que tener descripción, al menos una foto y los datos particulares de cada tipología, esto es, en el caso de los pisos tiene que tener también tamaño de vivienda, en el de los chalets, tamaño de vivienda y de jardín. Además, excepcionalmente, en los garajes no es necesario que el anuncio tenga descripción. Si el anuncio tiene todos los datos anteriores, proporciona otros 40 puntos.
+         */
     //TODO añade url del endpoint
     public ResponseEntity<Void> calculateScore() {
         //TODO rellena el cuerpo del método
---
+++ src/main/java/com/idealista/ranking/controller/Consts.java
@@ -0,0 +1,10 @@
+package com.idealista.ranking.controller;
+
+public final class Consts {
+    private Consts() {
+        throw new IllegalStateException("Constants class");
+    }
+
+    public static final String API_VERSION = "/v1";
+    public static final String ADS_ENDPOINT = "/ads";
+}
